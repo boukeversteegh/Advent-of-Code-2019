864dd3ef72aa499dffb746bc6644f1fd03bc84df
Bouke Versteegh <info@boukeversteegh.nl>
2019-12-01 06:56:57 +0100

    Day 1 recursive

--- day01/day01.py
+++ day01/day01.py
@@ -10,22 +10,19 @@ modules = \
 
 
 def fuel_required(mass):
-  total_fuel = base_fuel_required(mass)
-  additional_fuel = total_fuel
-  while True:
-    print("Fuel so far %s" % total_fuel)
-    additional_fuel = base_fuel_required(additional_fuel)
+  base_fuel = base_fuel_required(mass)
 
-    if additional_fuel <= 0:
-      return total_fuel
+  if base_fuel < 1:
+    return 0
 
-    total_fuel += additional_fuel
+  extra_fuel = fuel_required(base_fuel)
+  return base_fuel + extra_fuel
 
 
 total_fuel = sum([fuel_required(module) for module in modules])
 
 print(total_fuel)
 
-# 4941976
 # 4944796
 # 4944797
+# 4941976  <-- part 2 correct
