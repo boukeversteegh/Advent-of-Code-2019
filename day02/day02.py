21b8edbfa769c2707ca3a84e6943885e9644c6a4
Bouke Versteegh <info@boukeversteegh.nl>
2019-12-02 06:42:11 +0100

    Day 2 part 2

--- day02/day02.py
+++ day02/day02.py
@@ -10,15 +10,18 @@
 input_program = [int(code) for code in open("input.txt")
   .readlines()[0].strip().split(',')]
 
+instruction_pointer = 0
+positions = []
 
-def reset():
-  global positions
-  positions = input_program
+
+def load_program():
+  global positions, instruction_pointer
+  positions.clear()
+  positions.extend(input_program)
   positions[1] = 12
   positions[2] = 2
+  instruction_pointer = 0
 
-
-reset()
 
 # Example
 # P0 = [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]
@@ -26,11 +29,6 @@ P1 = [1, 0, 0, 0, 99]
 P2 = [2, 3, 0, 3, 99]
 P3 = [2, 4, 4, 5, 99, 0]
 P4 = [1, 1, 1, 4, 99, 5, 6, 0, 99]
-
-# positions
-
-instruction_pointer = 0
-
 
 def get_parameters(position):
   global arg_c, val_a, val_b
@@ -43,9 +41,7 @@ def get_parameters(position):
 
   val_a = positions[pos_a]
   val_b = positions[pos_b]
-  print("OP A=%s B=%s -> C=%s" % (arg_a, arg_b, pos_c))
   return val_a, val_b, pos_c
-
 
 def print_positions(in_positions):
   print(",".join([str(code) for code in in_positions]))
@@ -53,29 +49,57 @@ def print_positions(in_positions):
 
 print_positions(positions)
 
-while True:
-  opcode = positions[instruction_pointer]
 
-  if opcode == 1:
-    # add A + B -> C
-    (val_a, val_b, arg_c) = get_parameters(instruction_pointer)
+def run_program():
+  global val_a, val_b, arg_c, instruction_pointer
+  while True:
+    opcode = positions[instruction_pointer]
 
-    val_c = val_a + val_b
-    positions[arg_c] = val_c
-    instruction_pointer += 4
+    if opcode == 1:
+      # add A + B -> C
+      (val_a, val_b, arg_c) = get_parameters(instruction_pointer)
 
-  if opcode == 2:
-    # multiply A * B -> C
-    (val_a, val_b, arg_c) = get_parameters(instruction_pointer)
+      val_c = val_a + val_b
+      positions[arg_c] = val_c
+      instruction_pointer += 4
 
-    val_c = val_a * val_b
-    positions[arg_c] = val_c
-    instruction_pointer += 4
+    if opcode == 2:
+      # multiply A * B -> C
+      (val_a, val_b, arg_c) = get_parameters(instruction_pointer)
 
-  if opcode == 99:
-    # instruction_pointer += 1
-    break
+      val_c = val_a * val_b
+      positions[arg_c] = val_c
+      instruction_pointer += 4
+
+    if opcode == 99:
+      # instruction_pointer += 1
+      break
+
+    print(instruction_pointer)
+    # print_positions(positions)
+
+
+target_output = 19690720
+
+
+def part2():
+  for noun in range(0, 100):
+    for verb in range(0, 100):
+      load_program()
+      positions[1] = noun
+      positions[2] = verb
+      print_positions(positions)
+      run_program()
+      result = positions[0]
+
+      # print("VERB: %s/100" % verb)
+
+      if result == target_output:
+        print("NOUN=%s, VERB=%s, Answer: %s" % (noun, verb, 100 * noun + verb))
+        return
+    # print("NOUN: %s/100" % noun)
 
   print_positions(positions)
 
-print_positions(positions)
+
+part2()
