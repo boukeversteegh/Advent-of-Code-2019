3a461f03145e623b5c0947971a38558c0472839a
Bouke Versteegh <info@boukeversteegh.nl>
2019-12-03 06:46:03 +0100

    Day 3 - Refactor into Class

--- day03/day03.py
+++ day03/day03.py
@@ -8,77 +8,82 @@ raw = open('input.txt').readlines()
 
 lines = [line.strip() for line in raw]
 
-grid = collections.defaultdict(lambda: collections.defaultdict(lambda: {}))
 
-for line_index, line in enumerate(lines):
-  sections = line.split(',')
+class Intersector:
 
-  x, y = (0, 0)
-  line_steps_total = 0
-  for section in sections:
-    section_direction = section[0]
-    section_dist = int(section[1:])
+  def __init__(self) -> None:
+    super().__init__()
 
-    section_dx = 0
-    section_dy = 0
+    self.grid = collections.defaultdict(lambda: collections.defaultdict(lambda: {}))
 
-    if section_direction == 'U':
-      section_dy -= 1
-    if section_direction == 'D':
-      section_dy += 1
-    if section_direction == 'L':
-      section_dx -= 1
-    if section_direction == 'R':
-      section_dx += 1
+  def solve(self):
+    for line_index, line in enumerate(lines):
+      sections = line.split(',')
 
-    section_tx, section_ty = (x + section_dx, y + section_dy)
+      x, y = (0, 0)
+      line_steps_total = 0
+      for section in sections:
+        section_direction = section[0]
+        section_dist = int(section[1:])
 
-    for i in range(0, section_dist):
-      x += section_dx
-      y += section_dy
-      line_steps_total += 1
+        section_dx = 0
+        section_dy = 0
 
-      if line_index not in grid[y][x].keys():
-        grid[y][x][line_index] = line_steps_total
+        if section_direction == 'U':
+          section_dy -= 1
+        if section_direction == 'D':
+          section_dy += 1
+        if section_direction == 'L':
+          section_dx -= 1
+        if section_direction == 'R':
+          section_dx += 1
+
+        section_tx, section_ty = (x + section_dx, y + section_dy)
+
+        for i in range(0, section_dist):
+          x += section_dx
+          y += section_dy
+          line_steps_total += 1
+
+          if line_index not in self.grid[y][x].keys():
+            self.grid[y][x][line_index] = line_steps_total
+
+  def part1(self):
+    min_x = 0
+    min_y = 0
+    min_distance = None
+
+    for y, row in self.grid.items():
+      for x, lines in row.items():
+        if len(lines.items()) > 1:
+          distance = abs(x) + abs(y)
+          if min_distance is None or (distance < min_distance):
+            print("Intersection at %s" % (((x, y), distance),))
+            min_x = x
+            min_y = y
+            min_distance = distance
+
+    print(((min_x, min_y), min_distance))
+
+  def part2(self):
+    min_x = 0
+    min_y = 0
+    min_time = None
+    for y, row in self.grid.items():
+      for x, line_times in row.items():
+        if len(line_times.items()) > 1:
+          time = sum(line_times.values())
+          if min_time is None or (time < min_time):
+            min_time = time
+            min_x = x
+            min_y = y
+    print(((min_x, min_y), min_time))
 
 
-def part1():
-  min_x = 0
-  min_y = 0
-  min_distance = None
-
-  for y, row in grid.items():
-    for x, lines in row.items():
-      if len(lines.items()) > 1:
-        distance = abs(x) + abs(y)
-        if min_distance is None or (distance < min_distance):
-          print("Intersection at %s" % (((x, y), distance),))
-          min_x = x
-          min_y = y
-          min_distance = distance
-
-  print(((min_x, min_y), min_distance))
-
-
-def part2():
-  min_x = 0
-  min_y = 0
-  min_distance = None
-  min_time = None
-  for y, row in grid.items():
-    for x, line_times in row.items():
-      if len(line_times.items()) > 1:
-        distance = abs(x) + abs(y)
-        time = sum(line_times.values())
-        if min_time is None or (time < min_time):
-          min_time = time
-          min_x = x
-          min_y = y
-  print(((min_x, min_y), min_time))
-
-
-part1()
-part2()
+intersector = Intersector()
+intersector.solve()
+intersector.part1()
+intersector.part2()
 
 # 2923 WRONG
 # 399  RIGHT
