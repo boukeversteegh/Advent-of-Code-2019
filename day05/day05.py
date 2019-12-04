004fa84099829cb767a8b54405418bb976171c44
Bouke Versteegh <info@boukeversteegh.nl>
2019-12-04 23:05:11 +0100

    Day 5 - Template setup

---
+++ day05/day05.py
@@ -0,0 +1,34 @@
+import unittest
+
+
+class Day5:
+  def __init__(self):
+    pass
+
+
+class Day5Part1(Day5):
+  def solve(self, args):
+    pass
+
+
+class Day5Part2(Day5):
+  def solve(self, args):
+    pass
+
+
+class Examples(unittest.TestCase):
+  def test_examples_part1(self):
+    day5 = Day5Part1()
+    # self.assertTrue()
+
+  def test_examples_part1(self):
+    day5 = Day5Part1()
+    # self.assertTrue()
+
+
+class Solutions(unittest.TestCase):
+  def test_part1(self):
+    day5 = Day5Part1()
+
+  def test_part2(self):
+    day5 = Day5Part2()
