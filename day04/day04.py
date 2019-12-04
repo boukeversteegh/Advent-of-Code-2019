aae5f1c78d898d8a1d852a76d4c7eb35083097f9
Bouke Versteegh <info@boukeversteegh.nl>
2019-12-04 06:43:40 +0100

    Day 4 - Part 2

--- day04/day04.py
+++ day04/day04.py
@@ -14,18 +14,32 @@ class Day4:
     return valid_count
 
   def is_valid(self, password):
+    password = str(password)
     if len(password) != 6:
       return False
 
     have_double = False
     is_monotonous = True
-    for p in range(0, 5):
+    current_sequence = ''
+    sequence_length = 0
 
-      if password[p] == password[p + 1]:
-        have_double = True
+    for p in range(0, 6):
+      digit = password[p]
 
-      if password[p] > password[p + 1]:
+      if digit != current_sequence:
+        if sequence_length == 2:
+          have_double = True
+
+        current_sequence = digit
+        sequence_length = 1
+      else:
+        sequence_length += 1
+
+      if p < 5 and digit > password[p + 1]:
         is_monotonous = False
+
+    if sequence_length == 2:
+      have_double = True
 
     if not have_double:
       return False
@@ -42,10 +56,10 @@ class Tests(unittest.TestCase):
     super().__init__(methodName)
     self.day4 = Day4()
 
-  def test_examples(self):
-    self.assertTrue(self.day4.is_valid('111111'))
+  def test_examples_part1(self):
+    self.assertFalse(self.day4.is_valid('111111'))
     self.assertTrue(self.day4.is_valid('456779'))
-    self.assertTrue(self.day4.is_valid('000000'))
+    self.assertFalse(self.day4.is_valid('000000'))
     self.assertFalse(self.day4.is_valid('223450'))
     self.assertFalse(self.day4.is_valid(''))
     self.assertFalse(self.day4.is_valid('123'))
@@ -54,15 +68,38 @@ class Tests(unittest.TestCase):
     self.assertFalse(self.day4.is_valid('123789'))
     self.assertFalse(self.day4.is_valid('338879'))
 
+  def test_examples_part2(self):
+    valid = [
+      '112233',
+      '111122',
+      '334566',
+    ]
+    invalid = [
+      '111111',
+      '123444',
+      '444567',
+      '000123',
+    ]
+    for v in valid:
+      self.assertTrue(self.day4.is_valid(v), msg="%s should be valid" % v)
+
+    for i in invalid:
+      self.assertFalse(self.day4.is_valid(i), msg="%s should be INVALID" % v)
+
   def test_part1(self):
     day4 = Day4()
     valid_count = day4.part1()
     print(valid_count)
 
 
+# Part 1 1150
+# Part 2 648 BAD
+# Part 2 454 BAD
+# Part 2 510 BAD
+
 def main():
   day4 = Day4()
-  valid_count = day4.part1()
+  valid_count = day4.part2()
   print(valid_count)
 
 
