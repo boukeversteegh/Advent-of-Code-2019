1728ef20c08528b969ae01fa0823857463bd3264
Bouke Versteegh <info@boukeversteegh.nl>
2019-12-07 22:51:54 +0100

    Day 8 - template

---
+++ day08/day08.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class Day8:
+  pass
+
+
+class ExamplesDay8(unittest.TestCase):
+
+  def test_part1(self):
+    pass
+
+
+class SolutionsDay8(unittest.TestCase):
+  def test_part1(self):
+    pass
+
+  def test_part2(self):
+    pass
