c245bca75d1ccc736c423bd90c81a91bf426c05c
Bouke Versteegh <info@boukeversteegh.nl>
2019-12-10 19:04:28 +0100

    Day 10 - part 2 solved

--- day10/day10.py
+++ day10/day10.py
@@ -1,6 +1,22 @@
 import collections
 import math
 
+
+def print_map(asteroids, groups):
+  for y, row in enumerate(rows):
+    for (x, pos) in enumerate(row):
+      if (x, y) in asteroids:
+
+        a_groups = [key for key, group in groups.items() if (x, y) in group]
+        if a_groups != []:
+          print(chr(65 + list(groups.keys()).index(a_groups[0])), end='')
+        else:
+          print('#', end='')
+      else:
+        print('.', end='')
+    print('')
+  print('-----')
+
 with open('day10/input.txt') as fh:
   rows = [line.strip() for line in fh.readlines()]
   w = len(rows[0])
@@ -47,28 +63,23 @@ with open('day10/input.txt') as fh:
 
         groups[group].append((ox, oy))
 
-    # print(groups)
+    print((cx, cy))
 
-    # print((cx, cy))
-    # for y, row in enumerate(rows):
-    #   for (x, pos) in enumerate(row):
-    #     if (x, y) in asteroids:
-    #
-    #       a_groups = [key for key, group in groups.items() if (x, y) in group]
-    #       if a_groups != []:
-    #         print(chr(65 + list(groups.keys()).index(a_groups[0])), end='')
-    #       else:
-    #         print('#', end='')
-    #     else:
-    #       print('.', end='')
-    #   print('')
-    # print('-----')
+    # print_map(asteroids, groups)
 
-    # print('Group/asteroid count: %s' % len(groups))
     candidate_group_counts.append([(cx, cy), len(groups)])
     candidate_groups[(cx, cy)] = groups
 
   # print(candidate_group_counts)
+
+  # part 1
+
+  # 264 BAD
+  # 265 BAD
+  # 272 BAD #5
+  # 274 BAD <--- Right answer probably lol
+  # 289 BAD
+  # 290 BAD
 
   # Part 2
   (x, y), count = max(candidate_group_counts, key=lambda entry: entry[1])
@@ -88,35 +99,41 @@ with open('day10/input.txt') as fh:
     bx, by = b
     dx = ax - bx
     dy = ay - by
-    return math.atan2(dy, dx)
+    return (math.degrees(math.atan2(dy, dx)) - 90 + 360) % 360
 
 
   groups = [sorted(group, key=lambda group: dist(group, (x, y))) for group in groups.values()]
 
   groups = sorted(groups, key=lambda group: angle((x, y), group[0]))
 
-  count = 0
 
-  while True:
-    for group in groups:
-      if group == []:
-        continue
-      print(group)
-      nearest = group.pop(0)
-
-      count += 1
-      print(count, nearest)
-
-      if count == 200:
-        print("DONE", nearest)
+  def part2(groups):
+    count = 0
+    while True:
+      print_map(asteroids, {index: group for index, group in enumerate(groups)})
+      groups = [group for group in groups if len(group) > 0]
+      if (len(groups) == 0):
         break
 
-      # 801
+      print("Iterating over %s non-empty groups" % (len(groups)))
+
+      for group in groups:
+        if group == []:
+          continue
+
+        print(group)
+        nearest = group.pop(0)
+        print("popped %s" % (nearest,))
+        print_map(asteroids, {index: group for index, group in enumerate(groups)})
+
+        count += 1
+        # print(count, nearest)
+
+        if count == 200:
+          print("DONE", nearest)
+          return
 
 
-  # 264 BAD
-  # 265 BAD
-  # 272 BAD #5
-  # 274 BAD <--- Right answer probably lol
-  # 289 BAD
-  # 290 BAD
+  part2(groups)
+
+  # (3, 5) -> 305 Correct
